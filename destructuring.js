45cf6cb0d81cb5b2e68366886ad252a5c4ef04b5
Faugnom1 <128656243+Faugnom1@users.noreply.github.com>
2023-06-07 20:39:15 -0400

    Create destructuring.js

---
+++ destructuring.js
@@ -0,0 +1,13 @@
+const obj = {
+  numbers: {
+    a: 1,
+    b: 2
+  }
+};
+
+const {a,b} = obj.numbers
+
+[arr[0], arr[1]] = [arr[1], arr[0]]
+
+raceResults(['Tom', 'Margaret', 'Allison', 'David', 'Pierre'])
+const raceResults = ([first, second, third, ...rest]) => ({first, second, third, rest})
